5c26f77e7f54aae93b4051e3a6e0a36ecbeacf83
zxh451200 <460233345@qq.com>
2023-11-27 22:29:28 +0800

    3

--- src/main/java/com/example/xinhua/mapper/UserMapper.java
+++ src/main/java/com/example/xinhua/mapper/UserMapper.java
@@ -7,7 +7,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
-import com.example.xinhua.pojo.PageBean;
 import com.example.xinhua.pojo.UserPojo;
 
 @Mapper
