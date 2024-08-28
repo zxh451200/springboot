06eb878900362ca0687c0372fccd9df216b37a70
华少 <460233345@qq.com>
2024-08-28 21:54:25 +0800

    1

--- src/main/java/com/example/xinhua/XinhuaApplication.java
+++ src/main/java/com/example/xinhua/XinhuaApplication.java
@@ -10,15 +10,35 @@ import org.springframework.context.ApplicationListener;
 @SpringBootApplication
 public class XinhuaApplication implements ApplicationListener<ApplicationReadyEvent> {
 
-	public static void main(String[] args) {
-		Logger Logger = LoggerFactory.getLogger(XinhuaApplication.class);
-		Logger.info("AAA-启动前-AAA");
-		SpringApplication.run(XinhuaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Logger Logger = LoggerFactory.getLogger(XinhuaApplication.class);
+        Logger.info("AAA-启动前-AAA");
+        SpringApplication.run(XinhuaApplication.class, args);
 
-	@Override
-	public void onApplicationEvent(ApplicationReadyEvent event) {
-		System.out.println("BBB-启动了-BBB");
-	}
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println(BinSearch(arr, 3));
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        System.out.println("BBB-启动了-BBB");
+    }
+
+
+    //二分查找
+    public static int BinSearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int min = left + (right - left) / 2;
+            if (nums[min] == target) {
+                return min;
+            } else if (nums[min] < target) {
+                left = min + 1;
+            } else {
+                right = min - 1;
+            }
+        }
+        return -1;
+    }
 
 }
