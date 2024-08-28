4a0814a8eb207fe5017fb4c619bcb9a915a56864
华少 <460233345@qq.com>
2024-08-28 22:40:42 +0800

    1

--- src/main/java/com/example/xinhua/XinhuaApplication.java
+++ src/main/java/com/example/xinhua/XinhuaApplication.java
@@ -7,16 +7,70 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class XinhuaApplication implements ApplicationListener<ApplicationReadyEvent> {
 
-    public static void main(String[] args) {
+    static ThreadLocal<Object> threadLocal=new ThreadLocal<>();
+
+
+
+    public static void main(String[] args) throws Exception {
         Logger Logger = LoggerFactory.getLogger(XinhuaApplication.class);
         Logger.info("AAA-启动前-AAA");
         SpringApplication.run(XinhuaApplication.class, args);
 
         int[] arr = {1, 2, 3, 4, 5, 6};
         System.out.println(BinSearch(arr, 3));
+
+//        //线程池
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        for (int i = 0; i < 10; i++) {
+//            executorService.execute(()->{
+//                Byte[] bytes = new Byte[50240 * 10240];//50m
+//                threadLocal.set(bytes);
+//                //内存泄漏的处理方法
+//                threadLocal.remove();
+//            });
+//        }
+//        executorService.shutdown();
+//        try {
+//            executorService.awaitTermination(1, TimeUnit.MINUTES);
+//        } catch (Exception e) {
+//            throw new Exception(e);
+//        }
+
+
+        //线程学习
+        Thread thread1 = new Thread(() -> {
+            System.out.println(threadLocal.get());
+            threadLocal.set(111);
+            System.out.println(threadLocal.get());
+            System.out.println("线程启动1");
+        },"thread-1");
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("线程启动2");
+        },"thread-2");
+
+        thread2.start();
+        try{
+            thread2.join();
+        }catch (Exception e){
+            System.out.println("线程启动异常");
+        }
+        thread1.start();
+        System.out.println("线程名字是:"+thread1.getName());;
+
+
+
     }
 
     @Override
