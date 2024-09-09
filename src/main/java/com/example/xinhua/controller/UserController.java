6e05994ace30aa12706f41492c27de0f23a820f1
zxh451200 <31895103+zxh451200@users.noreply.github.com>
2024-09-09 17:19:16 +0800

    Update UserController.java

--- src/main/java/com/example/xinhua/controller/UserController.java
+++ src/main/java/com/example/xinhua/controller/UserController.java
@@ -104,4 +104,20 @@ public class UserController {
         System.out.println(str.charAt(index));
     }
 
+    // 二分查找
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
 }
