188f4ae069229d4acbbf5d016b2de67e444c3a01
zxh451200 <31895103+zxh451200@users.noreply.github.com>
2024-09-11 18:49:59 +0800

    杨辉三角

--- src/main/java/com/example/xinhua/controller/Yanghuisanjiao.java
+++ src/main/java/com/example/xinhua/controller/Yanghuisanjiao.java
@@ -2,6 +2,8 @@ public class Main {
     //    杨辉三角
     public static void main(String[] args) {
         print(10);
+        System.out.println("-----------------------------------------------------");
+        printjiyi(10);
     }
 
     /**
@@ -16,6 +18,7 @@ public class Main {
         return sanjiao(i - 1, j - 1) + sanjiao(i - 1, j);
     }
 
+    // 普通
     public static void print(int n) {
         for (int i = 0; i < n; i++) {
             printSpace(n, i);
@@ -24,6 +27,34 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+
+    // 记忆法优化
+    public static void printjiyi(int n) {
+
+        int[][] ints = new int[n][];
+        for (int i = 0; i < n; i++) {
+            ints[i] = new int[i + 1];
+            printSpace(n, i);
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%-4d", sanjiaojiyi(ints, i, j));
+            }
+            System.out.println();
+        }
+    }
+
+    public static int sanjiaojiyi(int[][] ints, int i, int j) {
+        //计算过就不走了
+        if (ints[i][j] > 0) {
+            return ints[i][j];
+        }
+        if (j == 0 || i == j) {
+            ints[i][j] = 1;
+            return 1;
+        }
+        ints[i][j] = sanjiaojiyi(ints, i - 1, j - 1) + sanjiaojiyi(ints, i - 1, j);
+        return ints[i][j];
     }
 
     public static void printSpace(int n, int m) {
