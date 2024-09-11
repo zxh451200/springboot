2e8f6206c097f4dee2ec1d7a99c23ceb4af5f25c
zxh451200 <31895103+zxh451200@users.noreply.github.com>
2024-09-11 18:17:38 +0800

    杨辉三角

--- src/main/java/com/example/xinhua/controller/Yanghuisanjiao.java
+++ src/main/java/com/example/xinhua/controller/Yanghuisanjiao.java
@@ -1,9 +1,14 @@
-package com.xinhua.user.controller;
+public class Main {
+    //    杨辉三角
+    public static void main(String[] args) {
+        print(10);
+    }
 
-// 杨辉三角
-public class Yanghuisanjiao {
-
-
+    /**
+     * @param i 行坐标
+     * @param j 列坐标
+     * @return
+     */
     public static int sanjiao(int i, int j) {
         if (j == 0 || i == j) {
             return 1;
@@ -11,8 +16,20 @@ public class Yanghuisanjiao {
         return sanjiao(i - 1, j - 1) + sanjiao(i - 1, j);
     }
 
+    public static void print(int n) {
+        for (int i = 0; i < n; i++) {
+            printSpace(n, i);
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%-4d", sanjiao(i, j));
+            }
+            System.out.println();
+        }
+    }
 
-//    public static void main(String[] args) {
-//        System.out.println(sanjiao(4,2));
-//    }
+    public static void printSpace(int n, int m) {
+        int num = (n - 1 - m) * 2;
+        for (int i = 0; i < num; i++) {
+            System.out.print(" ");
+        }
+    }
 }
